2652a4af8ee438ac1493399a6bc1efcc5d326313
Chinmay Moharir <chinmay.moharir@gmail.com>
2019-11-11 07:30:46 -0500

    Revert "use error boundary scenario. Not to use around whole code. Just use o…"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 
 class App extends Component {
@@ -80,12 +79,12 @@ togglePersonsHandler = () => {
     persons = (
       <div>
         {this.state.persons.map((person, index) => {
-          return <ErrorBoundary><Person 
+          return <Person 
             click = {() => this.deletePersonHandler(index)}
             name = {person.name}
             age = {person.age}
             key = {person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)}/></ErrorBoundary>
+            changed={(event) => this.nameChangedHandler(event, person.id)}/>
         })}
 
         
